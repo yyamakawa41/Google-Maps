4871b747b9d29540d2a836d174057d07e93c4c5f
yyamakawa41 <yyamakawa@gmail.com>
2015-11-12 15:59:00 -0500

    Directions
    Directions and images

--- Google Map in Class/controller.js
+++ Google Map in Class/controller.js
@@ -13,8 +13,8 @@ var mapctl = angular.module('myApp', []).controller('mapCtrl', function($scope){
 	var infoWindow = new google.maps.InfoWindow();
 
 	var createMarker = function(city, index){
-		console.log(city);
-		console.log(index);
+		// console.log(city);
+		// console.log(index);
 
 		var latLon = city.latLon.split(',');
 		var lat = latLon[0];
@@ -25,7 +25,57 @@ var mapctl = angular.module('myApp', []).controller('mapCtrl', function($scope){
 			position: new google.maps.LatLng(lat, lon),
 			title: city.city,
 			icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7CFE7569'
-		})
+		});
+
+		markerContentHTML = '<div class="infoWindowContent">';
+	       markerContentHTML += '<div class="total-pop">Total Population: ' + city.yearEstimate + '</div>';
+	       markerContentHTML += '<div class="pop-dens-last-year">2010 Census: ' + city.lastCensus + '</div>';
+	       markerContentHTML += '<div class="pop-change">Population Change %: ' + city.change + '</div>';
+	       markerContentHTML += '<div class="pop-dens">Population Density: ' + city.lastPopDensity + '</div>';
+	       markerContentHTML += '<div class="state">State: ' + city.state + '</div>';
+	       markerContentHTML += '<div class="land-area">Land Area: ' + city.landArea + '</div>';
+	       markerContentHTML += '<a href="#" onclick="getDirections('+lat+','+lon+')">Get directions</a>';
+	    markerContentHTML += '</div>';
+
+	    marker.content = markerContentHTML;
+
+	    google.maps.event.addListener(marker, 'click', function(){
+	    	infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+	    	infoWindow.open($scope.map, marker);
+	    });
+
+	    $scope.markers.push(marker);
+
+	}
+
+	$scope.triggerClick = function(i){
+		google.maps.event.trigger($scope.markers[i-1], "click");
+	}
+
+	getDirections = function(lat, lon){
+		var directionsService = new google.maps.DirectionsService();
+   		var directionsDisplay = new google.maps.DirectionsRenderer();
+   		var map = new google.maps.Map(document.getElementById('map'),{
+   			zoom: 7,
+   			mapTypeId: google.maps.MapTypeId.ROADMAP
+   		});
+
+   		directionsDisplay.setMap(map);
+   		directionsDisplay.setPanel(document.getElementById('map-panel'));
+
+   		var request = {
+           //Origin hardcoded to Atlanta. Require geocode current loc,
+           //or give user input
+          origin: 'Atlanta, GA', 
+          destination:new google.maps.LatLng(lat,lon), 
+          travelMode: google.maps.DirectionsTravelMode.DRIVING
+        };
+
+        directionsService.route(request, function(response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+          }
+        });
 	}
 
 	$scope.cities = cities;
