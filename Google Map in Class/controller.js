5b314e7eaeb3a6aad3fd9a936e53e90ddef87c4a
yyamakawa41 <yyamakawa@gmail.com>
2015-11-12 16:34:56 -0500

    Update logo
    update logo for ATL

--- Google Map in Class/controller.js
+++ Google Map in Class/controller.js
@@ -20,11 +20,19 @@ var mapctl = angular.module('myApp', []).controller('mapCtrl', function($scope){
 		var lat = latLon[0];
 		var lon = latLon[1];
 
+		if(index == 0){
+			icon = 'assets/images/1.png';
+		}else if (index == 38){
+			icon = 'assets/images/atl.png';
+		}else{
+			icon = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7CFE7569'
+		}
+
 		var marker = new google.maps.Marker({
 			map: $scope.map,
 			position: new google.maps.LatLng(lat, lon),
 			title: city.city,
-			icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7CFE7569'
+			icon: icon
 		});
 
 		markerContentHTML = '<div class="infoWindowContent">';
