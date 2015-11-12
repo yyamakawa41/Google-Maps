5f1659992ed37aa06a0a7d433d447137bbf2b5da
yyamakawa41 <yyamakawa@gmail.com>
2015-11-12 13:57:19 -0500

    Setup

---
+++ Google Map in Class/controller.js
@@ -0,0 +1,10 @@
+
+angular.module('myApp', []).controller('mapCtrl', function($scope){
+
+	car mapOption = {
+		zoom: 4,
+		center: new google.maps.LatLng(40.0000, -98.0000),
+		mapTypeId: google.maps.mapTypeId.TERRAIN
+	}
+
+});
