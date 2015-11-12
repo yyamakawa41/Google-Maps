0e8908e5142ad036e5f25443d45c7b0eb58470c0
yyamakawa41 <yyamakawa@gmail.com>
2015-11-12 15:17:11 -0500

    Marker
    Marker Updates

--- Google Map in Class/controller.js
+++ Google Map in Class/controller.js
@@ -15,6 +15,17 @@ var mapctl = angular.module('myApp', []).controller('mapCtrl', function($scope){
 	var createMarker = function(city, index){
 		console.log(city);
 		console.log(index);
+
+		var latLon = city.latLon.split(',');
+		var lat = latLon[0];
+		var lon = latLon[1];
+
+		var marker = new google.maps.Marker({
+			map: $scope.map,
+			position: new google.maps.LatLng(lat, lon),
+			title: city.city,
+			icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7CFE7569'
+		})
 	}
 
 	$scope.cities = cities;
